9f00b3975d2bcad52628c12b53c5de568eb9443a
ybelm <yassinbelmaati@gmail.com>
2026-01-13 18:01:54 +0100

    Refactor n-gram key construction and map updates

--- ngrams.cpp
+++ ngrams.cpp
@@ -5,10 +5,8 @@
 void count_word_bigrams(const std::vector<std::string>& words, WordMap& map) {
     if (words.size() < 2) return;
     for (size_t i = 0; i + 1 < words.size(); ++i) {
-        std::string key = words[i];
-        key += " ";
-        key += words[i + 1];
-        map[key] = map[key] + 1;
+        std::string key = words[i] + " " + words[i + 1];
+        ++map[key];
     }
 }
 
@@ -16,36 +14,30 @@ void count_word_bigrams(const std::vector<std::string>& words, WordMap& map) {
 void count_word_trigrams(const std::vector<std::string>& words, WordMap& map) {
     if (words.size() < 3) return;
     for (size_t i = 0; i + 2 < words.size(); ++i) {
-        std::string key = words[i];
-        key += " ";
-        key += words[i + 1];
-        key += " ";
-        key += words[i + 2];
-        map[key] = map[key] + 1;
+        std::string key = words[i] + " " + words[i + 1] + " " + words[i + 2];
+        ++map[key];
     }
 }
 
 // Char bigrams
 void count_char_bigrams(const std::vector<char>& chars, WordMap& map) {
     if (chars.size() < 2) return;
-    std::string key = "";
+    std::string key(2, '\0');
     for (size_t i = 0; i + 1 < chars.size(); ++i) {
-        key = "";
-        key += chars[i];
-        key += chars[i + 1];
-        map[key] = map[key] + 1;
+        key[0] = chars[i];
+        key[1] = chars[i + 1];
+        ++map[key];
     }
 }
 
 // Char trigrams
 void count_char_trigrams(const std::vector<char>& chars, WordMap& map) {
     if (chars.size() < 3) return;
-    std::string key = "";
+    std::string key(3, '\0');
     for (size_t i = 0; i + 2 < chars.size(); ++i) {
-        key = "";
-        key += chars[i];
-        key += chars[i + 1];
-        key += chars[i + 2];
-        map[key] = map[key] + 1;
+        key[0] = chars[i];
+        key[1] = chars[i + 1];
+        key[2] = chars[i + 2];
+        ++map[key];
     }
 }
