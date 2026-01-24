4148532ee0ede22f71fcce4f1769439fc2214bcf
ybelm <yassinbelmaati@gmail.com>
2026-01-24 21:22:40 +0100

    Move source and header files to src and update build

--- preprocessing.h
+++
@@ -1,9 +0,0 @@
-#ifndef PREPROCESSING_H
-#define PREPROCESSING_H
-
-#include <string>
-
-// Normalize text (lowercase, remove punctuation and digits, collapse whitespace)
-std::string normalize(const std::string& text);
-
-#endif
